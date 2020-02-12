bdb6e1d8b5bd3336cb3a440bb5daa7e94a1e0218
Enrique Guerrero <enriqueguefer@gmail.com>
2020-02-12 21:32:53 +0100

    Rutas API REST de Trips

---
+++ api/routes/tripRoutes.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = function(app) {
+  var trips = require('../controllers/tripController');
+
+  app.route('/trips')
+	.get(trips.list_all_trips)
+	.post(trips.create_a_trip);
+
+  app.route('/trips/:tripId')
+    .get(trips.read_a_trip)
+	.put(trips.update_a_trip)
+    .delete(trips.delete_a_trip);
+};
