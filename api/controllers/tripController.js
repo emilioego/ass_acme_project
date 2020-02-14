6ca0d5fe4ac3e89bcdcb855009c956e98a36afff
Enrique Guerrero <enriqueguefer@gmail.com>
2020-02-14 19:10:55 +0100

    Método para procesar los errores

--- api/controllers/tripController.js
+++ api/controllers/tripController.js
@@ -1,6 +1,13 @@
 'use strict';
 /*---------------TRIP----------------------*/
 
+//ERROR RESPONSES
+const VALIDATION_ERROR='ValidationError';
+
+const STATUS_CODE_VALIDATION_ERROR=422;   
+const STATUS_CODE_NOT_FOUND=404;  
+const STATUS_CODE_INTERNAL_SERVER_ERROR=500;
+
 /*---------------GET----------------------*/
 var mongoose = require('mongoose'),
  Trip = mongoose.model('Trips');
@@ -10,7 +17,7 @@ exports.list_all_trips = function(req, res) {
     Trip.find({}, function(err, trips){
         if(err){
             console.error(Date(), ` ERROR: - GET /trips , Some error ocurred while retrieving trips: ${err.message}`);
-            res.send(err);
+            return processErrors(res, err);
         }else{
             console.log(Date(), ` SUCCESS: -GET /trips`);
             res.json(trips);
@@ -23,7 +30,7 @@ exports.read_a_trip = function (req, res) {
     Trip.findById(req.params.tripId, function(err, trip){
         if(err){
             console.error(Date(), ` ERROR: - GET /trips/${req.params.tripId} , Some error ocurred while retrieving a trip : ${err.message}`);
-            res.send(err);
+            return processErrors(res, err);
         }else{
             console.log(Date(), ` SUCCESS: -GET /trips/${req.params.tripId}`);
             res.json(trip);
@@ -39,10 +46,10 @@ exports.create_a_trip = function (req, res) {
     new_trip.save(function(err, trip) {
         if(err){
             console.error(Date(), ` ERROR: - POST /trips , Some error ocurred while saving a trip: ${err.message}`);
-            res.send(err);
+            return processErrors(res, err);
         }else{
             console.log(Date(), ` SUCCESS: -POST /trips`);
-            res.json(trips);
+            res.json(trip);
         }
     });
 };
@@ -54,7 +61,7 @@ exports.update_a_trip = function(req, res) {
     Trip.findOneAndUpdate({_id: req.params.tripId}, req.body, {new: true}, function(err, trip) {
         if(err){
             console.error(Date(), ` ERROR: - PUT /trips/${req.params.tripId} , Some error ocurred while updating a trip : ${err.message}`);
-            res.send(err);
+            return processErrors(res, err);
         }else{
             console.log(Date(), ` SUCCESS: -PUT /trips/${req.params.tripId}`);
             res.json(trip);
@@ -69,10 +76,21 @@ exports.delete_a_trip = function(req, res) {
     Trip.deleteOne({_id: req.params.tripId}, function(err, trip) {
         if(err){
             console.error(Date(), ` DELETE: - DELETE /trips/${req.params.tripId} , Some error ocurred while deleting a trip : ${err.message}`);
-            res.send(err);
+            return processErrors(res, err);
         }else{
             console.log(Date(), ` SUCCESS: -DELETE /trips/${req.params.tripId}`);
             res.json({ message: 'Trip successfully deleted' });
         }
     });
+}
+
+function processErrors(res, err){
+    switch(err.name){
+        case VALIDATION_ERROR:
+            return res.status(STATUS_CODE_VALIDATION_ERROR).send(err);
+        case VALIDATION_ERROR:
+            return res.status(STATUS_CODE_NOT_FOUND).send(err);
+        default:
+            return res.status(STATUS_CODE_INTERNAL_SERVER_ERROR).send(err);
+    }
 }
