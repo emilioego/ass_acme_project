f801fc602340d31436f362611caf0f9b556a1a50
Enrique Guerrero <enriqueguefer@gmail.com>
2020-02-14 19:10:16 +0100

    Añadido versionado a la api de trips

--- api/routes/tripRoutes.js
+++ api/routes/tripRoutes.js
@@ -2,11 +2,11 @@
 module.exports = function(app) {
   var trips = require('../controllers/tripController');
 
-  app.route('/trips')
+  app.route('/v1/trips')
 	.get(trips.list_all_trips)
 	.post(trips.create_a_trip);
 
-  app.route('/trips/:tripId')
+  app.route('/v1/trips/:tripId')
     .get(trips.read_a_trip)
 	.put(trips.update_a_trip)
     .delete(trips.delete_a_trip);
