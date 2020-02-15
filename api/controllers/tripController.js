487290bb9a5597c045dc29828d3ea900f129849c
Enrique Guerrero <enriqueguefer@gmail.com>
2020-02-15 01:06:00 +0100

    Actualizados varios bugs y códigos de estados del conntrolador de trip

--- api/controllers/tripController.js
+++ api/controllers/tripController.js
@@ -4,6 +4,8 @@
 //MONGOOSE ERROR RESPONSES
 const VALIDATION_ERROR='ValidationError';
 const CAST_ERROR='CastError';
+const OBJECT_ID_ERROR='ObjectId'
+const NOT_FOUND='NotFound'
  
 //RESPONSE_STATUS_CODE
 const CREATED=201;
@@ -22,7 +24,7 @@ exports.list_all_trips = function(req, res) {
     Trip.find({}, function(err, trips){
         if(err){
             console.error(Date(), ` ERROR: - GET /trips , Some error ocurred while retrieving trips: ${err.message}`);
-            return processErrors(res, err);
+            return processErrors(req, res, err);
         }else{
             console.log(Date(), ` SUCCESS: -GET /trips`);
             res.json(trips);
@@ -35,8 +37,12 @@ exports.read_a_trip = function (req, res) {
     Trip.findById(req.params.tripId, function(err, trip){
         if(err){
             console.error(Date(), ` ERROR: - GET /trips/${req.params.tripId} , Some error ocurred while retrieving a trip : ${err.message}`);
-            return processErrors(res, err);
+            return processErrors(req, res, err);
         }else{
+            if(!trip){
+                console.error(Date(), ` ERROR: - GET /trips/${req.params.tripId} , Not found trip with id : ${req.params.tripId}`);
+                return processErrors(req, res, {name: NOT_FOUND})
+            }
             console.log(Date(), ` SUCCESS: -GET /trips/${req.params.tripId}`);
             res.json(trip);
         }
@@ -51,7 +57,7 @@ exports.create_a_trip = function (req, res) {
     new_trip.save(function(err, trip) {
         if(err){
             console.error(Date(), ` ERROR: - POST /trips , Some error ocurred while saving a trip: ${err.message}`);
-            return processErrors(res, err);
+            return processErrors(req, res, err);
         }else{
             console.log(Date(), ` SUCCESS: -POST /trips`);
             res.status(CREATED).json(trip);
@@ -66,8 +72,12 @@ exports.update_a_trip = function(req, res) {
     Trip.findOneAndUpdate({_id: req.params.tripId}, req.body, {new: true}, function(err, trip) {
         if(err){
             console.error(Date(), ` ERROR: - PUT /trips/${req.params.tripId} , Some error ocurred while updating a trip : ${err.message}`);
-            return processErrors(res, err);
+            return processErrors(req, res, err);
         }else{
+            if(!trip){
+                console.error(Date(), ` ERROR: - PUT /trips/${req.params.tripId} , Not found trip with id : ${req.params.tripId}`);
+                return processErrors(req, res, {name: NOT_FOUND})
+            }
             console.log(Date(), ` SUCCESS: -PUT /trips/${req.params.tripId}`);
             res.json(trip);
         }
@@ -78,25 +88,31 @@ exports.update_a_trip = function(req, res) {
 
 exports.delete_a_trip = function(req, res) {
     console.log(Date(), ` -DELETE /trips/${req.params.tripId}`)
-    Trip.deleteOne({_id: req.params.tripId}, function(err, trip) {
+    Trip.findByIdAndRemove(req.params.tripId, function(err, trip) {
         if(err){
             console.error(Date(), ` DELETE: - DELETE /trips/${req.params.tripId} , Some error ocurred while deleting a trip : ${err.message}`);
-            return processErrors(res, err);
+            return processErrors(req, res, err);
         }else{
+            if(!trip){
+                console.error(Date(), ` ERROR: - DELETE /trips/${req.params.tripId} , Not found trip with id : ${req.params.tripId}`);
+                return processErrors(req, res, {name: NOT_FOUND})
+            }
             console.log(Date(), ` SUCCESS: -DELETE /trips/${req.params.tripId}`);
             res.status(NO_CONTENT).json({ message: 'Trip successfully deleted' });
         }
     });
 }
 
-function processErrors(res, err){
+function processErrors(req, res, err){
     switch(err.name){
         case VALIDATION_ERROR:
             return res.status(STATUS_CODE_VALIDATION_ERROR).send(err);
         case CAST_ERROR:
             return res.status(STATUS_CODE_CAST_ERROR).send(err);
-        case VALIDATION_ERROR:
+        case OBJECT_ID_ERROR:
             return res.status(STATUS_CODE_NOT_FOUND).send(err);
+        case NOT_FOUND:
+            return res.status(STATUS_CODE_NOT_FOUND).send({message: `Not found trip with id : ${req.params.tripId}`});
         default:
             return res.status(STATUS_CODE_INTERNAL_SERVER_ERROR).send(err);
     }
